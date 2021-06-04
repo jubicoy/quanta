f72bc42d5fb1dbec7dc027b3e73cf76bafd8b918
Arvo Koskikallio <arvo.koskikallio@jubic.fi>
2021-06-04 09:16:07 +0300

    Base for ImportWorkerImporter sample fetch

--- src/main/java/fi/jubic/quanta/external/importer/importworker/ImportWorkerImporter.java
+++ src/main/java/fi/jubic/quanta/external/importer/importworker/ImportWorkerImporter.java
@@ -1,23 +1,45 @@
 package fi.jubic.quanta.external.importer.importworker;
 
+import fi.jubic.quanta.dao.ImportWorkerDataSampleDao;
+import fi.jubic.quanta.dao.InvocationDao;
+import fi.jubic.quanta.dao.TaskDao;
 import fi.jubic.quanta.external.Importer;
 import fi.jubic.quanta.models.DataConnection;
 import fi.jubic.quanta.models.DataConnectionType;
 import fi.jubic.quanta.models.DataSample;
 import fi.jubic.quanta.models.DataSeries;
+import fi.jubic.quanta.models.ImportWorkerDataSample;
+import fi.jubic.quanta.models.Invocation;
+import fi.jubic.quanta.models.InvocationQuery;
+import fi.jubic.quanta.models.InvocationStatus;
+import fi.jubic.quanta.models.Task;
+import fi.jubic.quanta.models.TaskType;
 import fi.jubic.quanta.models.metadata.DataConnectionMetadata;
 import fi.jubic.quanta.models.typemetadata.TypeMetadata;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.ws.rs.NotFoundException;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 @Singleton
 public class ImportWorkerImporter implements Importer {
 
-    @Inject
-    ImportWorkerImporter() {
+    private final ImportWorkerDataSampleDao importWorkerDataSampleDao;
+    private final InvocationDao invocationDao;
+    private final TaskDao taskDao;
 
+    @Inject
+    ImportWorkerImporter(
+            ImportWorkerDataSampleDao importWorkerDataSampleDao,
+            InvocationDao invocationDao,
+            TaskDao taskDao) {
+
+        this.importWorkerDataSampleDao = importWorkerDataSampleDao;
+        this.invocationDao = invocationDao;
+        this.taskDao = taskDao;
     }
 
     @Override
@@ -37,6 +59,60 @@ public class ImportWorkerImporter implements Importer {
 
     @Override
     public DataSample getSample(DataSeries dataSeries, int rows) {
+
+        String taskName = dataSeries.getId() + "-" + dataSeries.getName()
+                + "-" + System.currentTimeMillis();
+
+        Task task = Task.builder()
+                .setId(-1L)
+                .setName(taskName)
+                .setTaskType(TaskType.IMPORT_SAMPLE)
+                .build();
+
+        taskDao.create(task);
+
+        Invocation invocation = Invocation.builder()
+                .setId(-1L)
+                .setInvocationNumber(-1L)
+                .setStatus(InvocationStatus.Pending)
+                .setTask(taskDao.getDetails(taskName)
+                        .orElseThrow(NotFoundException::new))
+                .build();
+
+        invocationDao.create(invocation);
+
+        try {
+            //We have to get the task/invocation from Dao,
+            //the IDs change once entered into the db
+
+            Invocation inv = invocationDao.search(
+                    new InvocationQuery().withTaskId(
+                            taskDao.getDetails(taskName)
+                                    .orElseThrow(NotFoundException::new)
+                                    .getId())
+                            .withStatus(InvocationStatus.Pending))
+                    .get(0);
+
+            while (true) {
+                Thread.sleep(5000);
+
+                Optional<ImportWorkerDataSample> importWorkerDataSample =
+                        importWorkerDataSampleDao.takeSample(inv.getId());
+
+                if (importWorkerDataSample.isPresent()) {
+                    return DataSample
+                            .builder()
+                            .setData(importWorkerDataSample.get().getData())
+                            .setDataSeries(dataSeries)
+                            .build();
+                }
+            }
+
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
