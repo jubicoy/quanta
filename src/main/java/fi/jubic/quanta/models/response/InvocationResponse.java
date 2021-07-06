d66ec375e78ef99cf3f6017bfcd7f8f8950d850e
Arvo Koskikallio <arvo.koskikallio@jubic.fi>
2021-07-06 13:27:03 +0300

    Added task to invocation response to simplify import workers

--- src/main/java/fi/jubic/quanta/models/response/InvocationResponse.java
+++ src/main/java/fi/jubic/quanta/models/response/InvocationResponse.java
@@ -1,10 +1,11 @@
 package fi.jubic.quanta.models.response;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.sun.istack.Nullable;
 import fi.jubic.easyvalue.EasyValue;
 import fi.jubic.quanta.models.Invocation;
 import fi.jubic.quanta.models.Parameter;
-
+import fi.jubic.quanta.models.Task;
 import java.util.List;
 
 @EasyValue
@@ -14,6 +15,9 @@ public abstract class InvocationResponse {
     public abstract Long getInvocationId();
 
     public abstract List<Parameter> getParameters();
+
+    @Nullable
+    public abstract Task getTask();
 
     public abstract Builder toBuilder();
 
@@ -29,6 +33,7 @@ public abstract class InvocationResponse {
         return InvocationResponse.builder()
                 .setInvocationId(invocation.getId())
                 .setParameters(invocation.getParameters())
+                .setTask(invocation.getTask())
                 .build();
     }
 }
