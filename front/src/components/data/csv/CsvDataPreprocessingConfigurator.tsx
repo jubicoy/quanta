732e82c4d90edbeb8568aeebf3814f0f51d995a7
Binh Vo <binh.vo@jubic.fi>
2021-08-06 13:10:16 +0300

    allow re-trigger sample

--- front/src/components/data/csv/CsvDataPreprocessingConfigurator.tsx
+++ front/src/components/data/csv/CsvDataPreprocessingConfigurator.tsx
@@ -1,13 +1,15 @@
 import React, { useState, useContext, useEffect } from 'react';
+import { Button } from '@material-ui/core';
 
 import { CsvColumnSelect } from '.';
-
 import { _DataConnectionConfiguratorContext } from '../DataImportPage';
 
 import {
   CsvDataSeriesConfiguration
 } from '../../../types';
+import { SampleResponse } from '../../../types/Api';
 import StepperButtons from '../StepperButtons';
+import { sample } from '../../../client';
 
 export const CsvDataPreprocessingConfigurator = () => {
   const {
@@ -16,7 +18,13 @@ export const CsvDataPreprocessingConfigurator = () => {
     dataSeries,
     setDataSeries,
 
+    setSampleResponse,
     sampleResponse,
+
+    setSampleData,
+
+    setSuccess,
+    setError,
 
     handleForward
   } = useContext(_DataConnectionConfiguratorContext);
@@ -48,6 +56,24 @@ export const CsvDataPreprocessingConfigurator = () => {
     }
   };
 
+  const handleSample = (): void => {
+    // Reset sample
+    setSampleResponse(null);
+    setSampleData([]);
+    if (dataSeries.dataConnection) {
+      sample(dataSeries.dataConnection.id, dataSeries)
+        .then((res: SampleResponse) => {
+          setDataSeries(res.dataSeries);
+          setSampleResponse(res);
+          setSampleData(res.data);
+          setSuccess('Fetch sample successfully');
+        })
+        .catch((e: Error) => {
+          setError('Sample failed to execute', e);
+        });
+    }
+  };
+
   return (uploadedData)
     ? (
       <>
@@ -56,6 +82,13 @@ export const CsvDataPreprocessingConfigurator = () => {
           handleCsvConfigurationChange={handleCsvConfigurationChange}
           handleCsvConfigurationReset={handleCsvConfigurationReset}
         />
+        <Button
+          color='primary'
+          variant='contained'
+          onClick={handleSample}
+        >
+          Fetch Sample
+        </Button>
         <StepperButtons
           onNextClick={handleForward}
           disableNext={!complete}
